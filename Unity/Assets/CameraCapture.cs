1fc6901c679e908b18f0d8e1cad95301160fd9ea
Graham Reeves <graham@grahamreeves.com>
2016-02-12 13:45:10 +0000

    Initial skeleton app using PopMovie to stream camera

---
+++ Unity/Assets/CameraCapture.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCapture : MonoBehaviour {
+
+	public Texture		mTarget;
+	public string		mFilename = "device:*";
+	public PopMovie		mMovie;
+
+	void Update () {
+	
+		if (mMovie == null) {
+			var Params = new PopMovieParams ();
+			mMovie = new PopMovie (mFilename, Params, true);
+			mMovie.AddDebugCallback( Debug.Log );
+		}
+
+		if (mMovie != null) {
+
+			mMovie.Update ();
+
+			if ( mTarget != null )
+			{
+				mMovie.UpdateTexture( mTarget, 0 );
+			}
+		}
+
+	}
+}
---
+++ Unity/Assets/FaceDetector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceDetector : MonoBehaviour {
+
+
+	public PopFace		mFaceDetector;
+	public Texture		mInput;
+	public UnityEngine.UI.Text	mOutput;
+
+	void Update () {
+
+		
+		if (mFaceDetector == null) {
+			mFaceDetector = new PopFace ();
+			mFaceDetector.AddDebugCallback( Debug.Log );
+		}
+		
+		if (mFaceDetector != null) {
+			
+			if ( mInput != null )
+			{
+				mFaceDetector.UpdateTexture( mInput );
+			}
+
+			if ( mOutput != null )
+			{
+				string Data = mFaceDetector.PopData();
+				if ( Data != null )
+					mOutput.text = Data;
+			}
+		}
+	
+	}
+}
